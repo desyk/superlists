86fb9a5221a1a806adc12e82e73099ee893faa26
Desyk <tackey@yandex.ru>
2015-08-05 19:21:28 +0300

    First commit: First FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+# browser = webdriver.Chrome('F:/Browsers/Chrome/chromedriver')
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title

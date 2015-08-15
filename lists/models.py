c8896ee64421bbe78673e45aeb0332b27c2268bd
Desyk <tackey@yandex.ru>
2015-08-15 12:20:02 +0300

    Add List model and list foreign key to Item

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 
-class Item(models.Model):
-	text = models.TextField(default='')
+class List(models.Model):
+    pass
 
-		
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)

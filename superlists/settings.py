f9049eb885235311cc95935b3be127a6b0b0f3d0
competitor <volodymyr.desyk@gmail.com>
2015-10-08 14:34:52 +0300

    Add local machine run option to settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm)y148o7u9n8*^1nweo&e@!^ur*52t5s&fex@32q@b@t=v)_wy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tack.pythonanywhere.com']
 
+# # For local machine uncomment:
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 # Application definition
 
